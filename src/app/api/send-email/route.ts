1fc3e6dd6e31c1ab167d7648ab09f7cf0b97aa87
iainbrux <me@iainbrux.com>
2026-03-31 08:55:10 +0100

    [feat] Commented back in second stage email as domain now verified

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -22,7 +22,7 @@ export async function POST(request: Request) {
     }: FormSubmission = await request.json();
     const { data, error } = await resend.batch.send([
       {
-        from: "Website Contact Form <onboarding@resend.dev>",
+        from: "Website Contact Form <contact@stalonebjj.co.uk>",
         html: `
         <h1>New Class Trial Request</h1>
         <h3>Professor Stalone,</h3>
@@ -40,37 +40,39 @@ export async function POST(request: Request) {
           Message: ${info}
         </p>
         <p>You can reply to this email directly to contact them.</p>
+        <p>Kind regards,</p>
+        <p>BC Digital</p>
       `,
         replyTo: email,
         subject: `New Class Trial Request ${new Date().toLocaleDateString()}`,
         to: "contact@stalonebjj.co.uk",
       },
-      // {
-      //   from: "onboarding@resend.dev",
-      //   html: `
-      //   <h1>We'll be in touch soon.</h1>
-      //   <p>Thanks for your interest in Stalone BJJ. We'll be in touch soon to arrange your trial class.</p>
-      //   <p>Here's the information you provided: please double check to ensure it's correct.</p>
-      //   <p>
-      //     Name: ${name}
-      //     <br />
-      //     Email: ${email}
-      //     <br />
-      //     Phone: ${number}
-      //     <br />
-      //     Class: ${selectedClass}
-      //     <br />
-      //     Message: ${info}
-      //   </p>
-      //   <p>Please email contact@stalonebjj.co.uk if you have further questions. Replies to this email are not monitored.</p>
-      //   <p>We're looking forward to welcoming you in to our school.</p>
-      //   <p>Kind regards,</p>
-      //   <p>Stalone BJJ Academy Chorley</p>
-      // `,
-      //   replyTo: "contact@stalonebjj.co.uk",
-      //   subject: `Thanks for your interest in Stalone BJJ`,
-      //   to: email,
-      // },
+      {
+        from: "Stalone BJJ Chorley <contact@stalonebjj.co.uk>",
+        html: `
+        <h1>We'll be in touch soon.</h1>
+        <p>Thanks for your interest in Stalone BJJ. We'll be in touch soon to arrange your trial class.</p>
+        <p>Here's the information you provided: please double check to ensure it's correct.</p>
+        <p>
+          Name: ${name}
+          <br />
+          Email: ${email}
+          <br />
+          Phone: ${number}
+          <br />
+          Class: ${selectedClass}
+          <br />
+          Message: ${info}
+        </p>
+        <p>If you have further questions, please reply to this email and we'll aim to get back to you as soon as we can.</p>
+        <p>We're looking forward to welcoming you in to our school.</p>
+        <p>Kind regards,</p>
+        <p>Stalone BJJ Academy Chorley</p>
+      `,
+        replyTo: "contact@stalonebjj.co.uk",
+        subject: `Contact Form Query Submitted ${new Date().toLocaleDateString()}`,
+        to: email,
+      },
     ]);
 
     console.log("Data:", data);
